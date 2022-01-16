b5400bbf9e02d2b5b6441c05e8b8fc8dcd521d51
lemuieux-gram <suwonvok@gmail.com>
2022-01-16 20:13:50 +0900

    second commit

--- script.js
+++ script.js
@@ -20,4 +20,16 @@ function handleTitleClick() {
     title.style.color ="blue";
 }
 
+function handleMouseEnter() {
+    console.log("mouse is here !");
+    title.innerText = "mouse is here!";
+}
+
+function handleMouseLeave() {
+    console.log("mouse is leave !");
+    title.innerText = "mouse is gone!";
+}
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
